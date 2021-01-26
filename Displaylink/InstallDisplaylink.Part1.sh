7a6caa63a72f0572350558e66ec480282fe3983c
Evan Washkow <epwashkow@gmail.com>
2021-01-25 20:00:26 -0800

    Remove Displaylink script

--- Displaylink/InstallDisplaylink.Part1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This installs the dependencies required to install Displaylink
-
-sudo dnf install dkms kernel-devel
-sudo dnf groupinstall "Development Tools"
-echo "Please Reboot Your Machine"
