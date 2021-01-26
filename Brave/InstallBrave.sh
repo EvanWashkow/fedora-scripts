c455a40256089d9928cce2bc3623f4ae9fc66760
Evan Washkow <epwashkow@gmail.com>
2021-01-25 19:32:29 -0800

    Restore InstallBrave script to the online instructions

--- Brave/InstallBrave.sh
+++ Brave/InstallBrave.sh
@@ -2,7 +2,7 @@
 
 # https://brave.com/linux/
 
-sudo dnf install -y dnf-plugins-core
+sudo dnf install dnf-plugins-core
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo dnf install -y brave-browser
+sudo dnf install brave-browser
